f05f4077f9a0d79e1770beb9d1b503e122310093
Rehan Manzoor <rehan@infamous.ae>
2017-01-07 14:18:20 +0400

    Changed model

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -3,24 +3,43 @@
 namespace RummyKhan\XLog\Models;
 
 if( config('xlog.connection') === 'mongodb' && class_exists('Jenssegers\Mongodb\Eloquent\Model') ){
-    use Jenssegers\Mongodb\Eloquent\Model;
+    class Log extends \Jenssegers\Mongodb\Eloquent\Model
+    {
+
+        protected $collection = 'logs';
+
+        protected $fillable = [
+            'id', 'title', 'url', 'response_code', 'session_id', 'ip', 'country', 'city',
+            'browser', 'browser_version', 'os', 'os_version', 'request_method', 'request_params',
+            'email', 'user_id',
+            'exception', 'trace', 'error_main', 'class', 'message', 'controller_action', 'is_redirect', 'redirected_to'
+        ];
+        protected $attributes = [
+            'redirected_to' => null,
+            'email' => null,
+            'user_id' => null
+        ];
+    }
 
 }else{
-    use Illuminate\Database\Eloquent\Model;
+    class Log extends \Illuminate\Database\Eloquent\Model
+    {
+
+        protected $collection = 'logs';
+
+        protected $fillable = [
+            'id', 'title', 'url', 'response_code', 'session_id', 'ip', 'country', 'city',
+            'browser', 'browser_version', 'os', 'os_version', 'request_method', 'request_params',
+            'email', 'user_id',
+            'exception', 'trace', 'error_main', 'class', 'message', 'controller_action', 'is_redirect', 'redirected_to'
+        ];
+
+        protected $attributes = [
+            'redirected_to' => null,
+            'email' => null,
+            'user_id' => null
+        ];
+    }
 }
-
-class Log extends Model
-{
-
-    protected $table = 'logs';
-
-    protected $fillable = [
-        'id', 'title', 'url', 'response_code', 'session_id', 'ip', 'country', 'city',
-        'browser', 'browser_version', 'os', 'os_version', 'request_method', 'request_params',
-        'email', 'user_id',
-        'exception', 'trace', 'error_main', 'class', 'message', 'controller_action', 'is_redirect', 'redirected_to'
-    ];
-}
-
 
 
