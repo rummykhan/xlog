dfcc4b005b94102eb55907220c51155066904758
Rehan Manzoor <rehan@infamous.ae>
2017-01-07 19:29:58 +0400

    Added support for deleting routes

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -6,14 +6,14 @@
             <div class="col-lg-12">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
-                        Website Requests Logs
+                        Logs
                     </div>
                     <table class="table table-striped table-responsive table-hover">
                         <thead>
                         <tr>
                             <th>Title</th>
+                            <th>Method</th>
                             <th>URL</th>
-                            <th>Code</th>
                             <th>IP</th>
                             <th>Country</th>
                             <th>City</th>
@@ -26,18 +26,27 @@
                         @foreach($logs as $log)
                             <tr>
                                 <td>{{ $log->title }}</td>
+                                <td>{{ $log->request_method }}</td>
                                 <td>{{ $log->url }}</td>
-                                <td>{{ $log->response_code }}</td>
                                 <td>{{ $log->ip }}</td>
                                 <td>{{ $log->country or '' }}</td>
                                 <td>{{ $log->city or '' }}</td>
                                 <td>{{ $log->os or '' }}</td>
                                 <td>{{ $log->browser or '' }}</td>
-                                <td><a href="{{ url("/rscpanel/logs/{$log->id}", [], env('SECURE')) }}" class="btn {{ $log->exception ? 'btn-danger' : 'btn-primary' }} btn-sm btn-flat">Detail</a></td>
+                                <td><a href="{{ url( str_replace('{id}', $log->id ?: $log->_id, config('xlog.routes.detail.route') )) }}" class="btn {{ $log->exception ? 'btn-danger' : 'btn-primary' }} btn-sm btn-flat">Detail</a></td>
+                                <td>
+                                    <form method="POST" action="{{ url( str_replace('{id}', $log->id ?: $log->_id, config('xlog.routes.delete.route') )) }}" name="log-delete-form">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <input type="submit" value="Delete" class="btn btn-primary btn-sm">
+                                    </form>
+                                </td>
                             </tr>
                         @endforeach
                         </tbody>
                     </table>
+                    <div class="text-center">
+                        {!! $logs->links() !!}
+                    </div>
                 </div>
             </div>
         </div>
